e5b588757b3dc5937703829870508bf08172b98e
Nacho Iacovino <nacho.iacovino@gmail.com>
2021-01-20 21:49:06 +0100

    Install react-router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
-import './App.css'
+import './App.css';
 import './CSS/Style.css';
+
+import { Route, Switch } from 'react-router-dom';
+
 import Header from './Components/Header/Header';
-import MobileNavigation from './Components/Mobile Navigation/MobileNavigation';
 import Main from './Components/Index/Main';
+import MetroCardList from './Components/Listings/MetroCardList';
+import MobileNavigation from './Components/Mobile Navigation/MobileNavigation';
 
 function App() {
   return (
-    <div className="App">
-     <Header />
-     <MobileNavigation />
-     <Main />
+    <div className='App'>
+      <Header />
+      <MobileNavigation />
+
+      <Switch>
+        <Route exact path='/'>
+          <Main />
+        </Route>
+        <Route exact path='/news'>
+          <MetroCardList />
+        </Route>
+        <Route exact path='/report'></Route>
+        <Route exact path='/notifications'></Route>
+        <Route exact path='/profile-page'></Route>
+      </Switch>
     </div>
   );
 }
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,34 +1,56 @@
-function Header(){
-    return(
-    <header className="fixed-top container-fluid bg-white d-none d-sm-block boder border-light shadow-sm">
-    <nav className="container navbar navbar-expand-lg navbar-expand-md navbar-expand-sm navbar-expand-xs navbar-light">
-      <div className= "d-flex flex-grow-1">
-        <span className="w-100 d-lg-none d-md-none d-sm-none d-block"></span>
-        <a className="navbar-brand d-none d-lg-inline-block d-md-inline-block d-sm-inline-block d-xs-inline-block" href="index.html"><img src='/assets/logo_image.svg' className="logo" alt="logo" /></a>
-      </div>
-      <div className="collapse navbar-collapse flex-grow-1 text-right">
-        <ul className="navbar-nav ml-auto flex-nowrap align-items-center">
-          <li className="nav-item mx-1">
-            <a className="nav-link navigation_link" href="/index.html">Home</a>
-          </li>
-          <li className="nav-item mx-1">
-            <a className="nav-link navigation_link" href="/news.html">News</a>
-          </li>
-          <li className="nav-item mx-1">
-            <a className="nav-link navigation_link" href="/report.html">Report</a>
-          </li>
-          <li className="nav-item mx-1">
-            <a className="nav-link navigation_icon" href="/notifications.html"><i className="fas fa-bell fa-lg"></i></a>
-          </li>
-          <li className="nav-item mx-1">
-            <a className="nav-link" href="/profile-page.html">
-              <img src="/assets/profile.png" className="rounded-circle border border-light" width="35" height="35" alt="profilepicture" />
-            </a>
-          </li>
-        </ul>
-      </div>
-    </nav>
-  </header>
-    );}
+import { Link } from 'react-router-dom';
+
+function Header() {
+  return (
+    <header className='fixed-top container-fluid bg-white d-none d-sm-block boder border-light shadow-sm'>
+      <nav className='container navbar navbar-expand-lg navbar-expand-md navbar-expand-sm navbar-expand-xs navbar-light'>
+        <div className='d-flex flex-grow-1'>
+          <span className='w-100 d-lg-none d-md-none d-sm-none d-block'></span>
+          <Link
+            className='navbar-brand d-none d-lg-inline-block d-md-inline-block d-sm-inline-block d-xs-inline-block'
+            to='index'
+          >
+            <img src='/assets/logo_image.svg' className='logo' alt='logo' />
+          </Link>
+        </div>
+        <div className='collapse navbar-collapse flex-grow-1 text-right'>
+          <ul className='navbar-nav ml-auto flex-nowrap align-items-center'>
+            <li className='nav-item mx-1'>
+              <Link className='nav-link navigation_link' to='/'>
+                Home
+              </Link>
+            </li>
+            <li className='nav-item mx-1'>
+              <Link className='nav-link navigation_link' to='/news'>
+                News
+              </Link>
+            </li>
+            <li className='nav-item mx-1'>
+              <Link className='nav-link navigation_link' to='/report'>
+                Report
+              </Link>
+            </li>
+            <li className='nav-item mx-1'>
+              <Link className='nav-link navigation_icon' to='/notifications'>
+                <i className='fas fa-bell fa-lg'></i>
+              </Link>
+            </li>
+            <li className='nav-item mx-1'>
+              <Link className='nav-link' to='/profile-page'>
+                <img
+                  src='/assets/profile.png'
+                  className='rounded-circle border border-light'
+                  width='35'
+                  height='35'
+                  alt='profilepicture'
+                />
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
+}
 
 export default Header;
